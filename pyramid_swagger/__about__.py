7e21b77d9ec6b6aa34ec90c3b6024b4b1b55a063
Scott Triglia <scott.triglia@gmail.com>
2014-05-29 16:14:24 -0700

    Bump version to 0.3.0

--- pyramid_swagger/__about__.py
+++ pyramid_swagger/__about__.py
@@ -12,7 +12,7 @@ __title__ = "pyramid_swagger"
 __summary__ = "Swagger tools for use in pyramid webapps"
 __uri__ = ""
 
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 
 __author__ = "Scott Triglia"
 __email__ = "striglia@yelp.com"
