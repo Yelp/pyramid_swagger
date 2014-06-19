de331caa0045c49d59bdde324b19324011e4f853
Scott Triglia <scott.triglia@gmail.com>
2014-06-18 23:31:06 -0700

    Make acceptance tests use webtest for more authentic testing

--- tests/acceptance/internal_test.py
+++ tests/acceptance/internal_test.py
@@ -2,7 +2,6 @@ import pytest
 import jsonschema.exceptions
 import pyramid_swagger
 import simplejson
-from pyramid.httpexceptions import HTTPClientError
 import mock
 from pyramid.httpexceptions import HTTPInternalServerError
 from pyramid.config import Configurator
@@ -37,132 +36,6 @@ def _validate_against_tween(request, response=None, settings=None):
     })
     registry = get_registry(settings=settings)
     validation_tween_factory(handler, registry)(request)
-
-
-def test_400_if_required_query_args_absent():
-    request = pyramid.testing.DummyRequest(
-        method='GET',
-        path='/sample/path_arg1/resource',
-        params={},
-        matchdict={'path_arg': 'path_arg1'},
-    )
-    with pytest.raises(HTTPClientError):
-        _validate_against_tween(request)
-
-
-def test_200_if_optional_query_args_absent():
-    request = pyramid.testing.DummyRequest(
-        method='GET',
-        path='/sample/path_arg1/resource',
-        params={'required_arg': 'test'},  # no `optional_arg` arg
-        matchdict={'path_arg': 'path_arg1'},
-    )
-    _validate_against_tween(request)
-
-
-def test_200_if_request_arg_is_wrong_type():
-    request = pyramid.testing.DummyRequest(
-        method='GET',
-        path='/sample/path_arg1/resource',
-        params={'required_arg': 1.0},
-        matchdict={'path_arg': 'path_arg1'},
-    )
-    with pytest.raises(HTTPClientError):
-        _validate_against_tween(request)
-
-
-def test_200_if_request_arg_types_are_not_strings():
-    request = pyramid.testing.DummyRequest(
-        method='GET',
-        path='/get_with_non_string_query_args',
-        params={
-            'int_arg': '5',
-            'float_arg': '3.14',
-            'boolean_arg': 'true',
-        }
-    )
-    _validate_against_tween(request)
-
-
-def test_400_if_path_not_in_swagger():
-    request = pyramid.testing.DummyRequest(
-        method='GET',
-        path='/does_not_exist',
-    )
-    with pytest.raises(HTTPClientError):
-        _validate_against_tween(request)
-
-
-@pytest.mark.xfail(reason='Issue #13')
-def test_400_if_path_arg_is_wrong_type():
-    request = pyramid.testing.DummyRequest(
-        method='GET',
-        path='/sample/invalid_arg/resource',
-        params={'required_arg': 'test'},
-        matchdict={'path_arg': 'invalid_arg'},
-    )
-    with pytest.raises(HTTPClientError):
-        _validate_against_tween(request)
-
-
-@pytest.mark.xfail(reason='Issue #13')
-def test_200_if_path_arg_types_are_not_strings():
-    request = pyramid.testing.DummyRequest(
-        method='GET',
-        path='/sample/nonstring/3/1.4/false',
-        matchdict={
-            'int_arg': '3',
-            'float_arg': '1.4',
-            'boolean_arg': 'false'
-        },
-    )
-    _validate_against_tween(request)
-
-
-def test_400_if_required_body_is_missing():
-    request = pyramid.testing.DummyRequest(
-        method='POST',
-        path='/sample_post',
-    )
-    with pytest.raises(HTTPClientError):
-        _validate_against_tween(request)
-
-
-def test_400_if_body_has_missing_required_arg():
-    request = pyramid.testing.DummyRequest(
-        method='POST',
-        path='/sample_post',
-        json_body={'bar': 'test'},
-    )
-    with pytest.raises(HTTPClientError):
-        _validate_against_tween(request)
-
-
-def test_200_if_body_has_missing_optional_arg():
-    request = pyramid.testing.DummyRequest(
-        method='POST',
-        path='/sample_post',
-        json_body={'foo': 'test'},
-    )
-    _validate_against_tween(request)
-
-
-def test_200_if_required_body_is_model():
-    request = pyramid.testing.DummyRequest(
-        method='POST',
-        path='/sample_post',
-        json_body={'foo': 'test', 'bar': 'test'},
-    )
-    _validate_against_tween(request)
-
-
-def test_200_if_required_body_is_primitives():
-    request = pyramid.testing.DummyRequest(
-        method='POST',
-        path='/post_with_primitive_body',
-        json_body=["foo", "bar"],
-    )
-    _validate_against_tween(request)
 
 
 def test_response_validation_disabled_by_default():
@@ -228,29 +101,6 @@ def test_500_when_response_arg_is_wrong_type():
     )
     with pytest.raises(HTTPInternalServerError):
         _validate_against_tween(request, response=response, settings=settings)
-
-
-def test_response_validation_success():
-    request = pyramid.testing.DummyRequest(
-        method='GET',
-        path='/sample/path_arg1/resource',
-        params={'required_arg': 'test'},
-        matchdict={'path_arg': 'path_arg1'},
-    )
-    data = {
-        'raw_response': 'foo',
-        'logging_info': {'foo': 'bar'}
-    }
-    settings = {
-        'pyramid_swagger.schema_path': 'tests/sample_swagger_spec.json',
-        'pyramid_swagger.enable_response_validation': True,
-        'pyramid_swagger.enable_swagger_spec_validation': False,
-    }
-    response = Response(
-        body=simplejson.dumps(data),
-        headers={'Content-Type': 'application/json; charset=UTF-8'},
-    )
-    _validate_against_tween(request, response=response, settings=settings)
 
 
 def test_pyramid_swagger_import():
