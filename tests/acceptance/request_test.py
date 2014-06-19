8014eceb122da1d64983345e0e40ccf6cc9d710c
Scott Triglia <scott.triglia@gmail.com>
2014-06-18 23:51:22 -0700

    Split acceptance tests into request vs response tests explicitly

--- tests/acceptance/request_test.py
+++ tests/acceptance/request_test.py
@@ -131,15 +131,3 @@ def test_200_if_required_body_is_primitives(test_app):
         expect_errors=True,
     )
     assert res.status_code == 200
-
-
-def test_200_for_normal_response_validation():
-    settings = {
-        'pyramid_swagger.schema_path': 'tests/acceptance/app/swagger.json',
-        'pyramid_swagger.enable_response_validation': True,
-    }
-    res = test_app(settings).post_json(
-        '/sample_post',
-        {'foo': 'test', 'bar': 'test'},
-    )
-    assert res.status_code == 200
