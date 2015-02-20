025ea5b8f34cc0db8f4d8e0278110e5c33b5b83b
Daniel Nephin <dnephin@yelp.com>
2015-02-19 17:08:30 -0800

    A small refactor of ingest.py to flatten out the calls from compile_swagger_schema

--- pyramid_swagger/ingest.py
+++ pyramid_swagger/ingest.py
@@ -21,53 +21,45 @@ class ApiDeclarationNotFoundError(Exception):
 
 
 def find_resource_names(api_docs_json):
-    return [
-        api['path'].lstrip('/')
-        for api in api_docs_json['apis']
-    ]
+    return [api['path'].lstrip('/') for api in api_docs_json['apis']]
 
 
-def build_schema_mapping(schema_dir):
+def build_schema_mapping(schema_dir, listing_json):
     """Discovers schema file locations and relations.
 
     :param schema_dir: the directory schema files live inside
     :type schema_dir: string
-    :returns: A tuple of (resource listing filepath, mapping) where the mapping
-        is between resource name and file path
-    :rtype: (string, dict)
+    :param listing_json: the contents of the listing document
+    :type  listing_json: string
+    :returns: a mapping from resource name to file path
+    :rtype: dict
     """
     def resource_name_to_filepath(name):
         return os.path.join(schema_dir, '{0}.json'.format(name))
 
-    listing, listing_json = _load_resource_listing(schema_dir)
-
-    return (
-        listing,
-        dict(
-            (resource, resource_name_to_filepath(resource))
-            for resource in find_resource_names(listing_json)
-        )
+    return dict(
+        (resource, resource_name_to_filepath(resource))
+        for resource in find_resource_names(listing_json)
     )
 
 
-def _load_resource_listing(schema_dir):
+def _load_resource_listing(resource_listing):
     """Load the resource listing from file, handling errors.
 
-    :param schema_dir: the directory schema files live inside
-    :type schema_dir: string
-    :returns: (resource listing filepath, resource listing json)
+    :param resource_listing: path to the api-docs resource listing file
+    :type  resource_listing: string
+    :returns: contents of the resource listing file
+    :rtype: dict
     """
-    resource_listing = os.path.join(schema_dir, API_DOCS_FILENAME)
     try:
         with open(resource_listing) as resource_listing_file:
-            resource_listing_json = simplejson.load(resource_listing_file)
+            return simplejson.load(resource_listing_file)
     # If not found, raise a more user-friendly error.
     except IOError:
         raise ResourceListingNotFoundError(
             'No resource listing found at {0}. Note that your json file '
             'must be named {1}'.format(resource_listing, API_DOCS_FILENAME)
         )
-    return resource_listing, resource_listing_json
 
 
 def compile_swagger_schema(schema_dir, should_validate_schemas):
@@ -79,35 +71,27 @@ def compile_swagger_schema(schema_dir, should_validate_schemas):
     :type should_validate_schemas: boolean
     :returns: a SwaggerSchema object
     """
-    listing, mapping = build_schema_mapping(schema_dir)
-    schema_resolvers = ingest_resources(
-        listing,
-        mapping,
-        schema_dir,
-        should_validate_schemas,
-    )
-    return SwaggerSchema(
-        listing,
-        mapping,
-        schema_resolvers,
-    )
+    listing_filename = os.path.join(schema_dir, API_DOCS_FILENAME)
+    listing_json = _load_resource_listing(listing_filename)
+    mapping = build_schema_mapping(schema_dir, listing_json)
+    schema_resolvers = ingest_resources(mapping, schema_dir)
+
+    if should_validate_schemas:
+        validate_swagger_schemas(listing_filename, mapping.values())
+
+    return SwaggerSchema(listing_filename, mapping, schema_resolvers)
 
 
-def ingest_resources(listing, mapping, schema_dir, should_validate_schemas):
+def ingest_resources(mapping, schema_dir):
     """Consume the Swagger schemas and produce a queryable datastructure.
 
-    :param listing: Filepath to a resource listing
-    :type listing: string
     :param mapping: Map from resource name to filepath of its api declaration
     :type mapping: dict
     :param schema_dir: the directory schema files live inside
     :type schema_dir: string
-    :param should_validate_schemas: if True, check schemas for correctness
-    :type should_validate_schemas: boolean
-    :returns: A list of SchemaAndResolver objects
+    :returns: A list of :class:`pyramid_swagger.load_schema.SchemaAndResolver`
+        objects
     """
-    resource_filepaths = mapping.values()
-
     ingested_resources = []
     for name, filepath in mapping.items():
         try:
@@ -121,11 +105,4 @@ def ingest_resources(listing, mapping, schema_dir, should_validate_schemas):
                 'your resource name and API declaration file do not '
                 'match?'.format(filepath, name, schema_dir)
             )
-
-    if should_validate_schemas:
-        validate_swagger_schemas(
-            listing,
-            resource_filepaths
-        )
-
     return ingested_resources
